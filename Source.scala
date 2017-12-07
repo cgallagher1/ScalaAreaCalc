d8634ab46c322f3a031fc3781f2e4a987a401b93
Charles Gallagher <cgallagher1@carthage.edu>
2017-12-07 13:54:14 -0600

    Implement User input

--- Source.scala
+++ Source.scala
@@ -2,4 +2,22 @@ object Source {
     def main(args: Array[String]) {
         printf("hello world")
     }
+    Class CoAndEx(var coefficient: Int, var exponential: Int){
+        def getCoefficient(): Int = coefficient
+        def getExponential(): Int = exponential
+
+        def setCoefficient(co: Int){
+            this.coefficient = co
+        }
+
+        def setExponential(ex: Int){
+            this.exponential = ex
+        }
+
+        def this(co: Int, ex: Int)
+        {
+            this.setCoefficient(co)
+            this.setExponential(ex)
+        }
+    }
 }
