72280d7ca0252f447b2ed7a1b153b8332964647a
Charles Gallagher <cgallagher1@carthage.edu>
2017-12-07 23:22:19 -0600

    Update Source.scala

--- Source.scala
+++ Source.scala
@@ -1,6 +1,7 @@
-import scala.io.StdIn
+import scala.io.StdIn._
 import scala.math._
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.BufferLike
 
 object Source {
     def main(args: Array[String]) {
@@ -15,9 +16,29 @@ object Source {
 
         //Beginging while statement
         while (answer == "y"){
-     
+            //Getting user input
+            print("Please enter the coefficient for term " + counter.toString + ": ")
+            var coefficient = readLine.toFloat
+            print("Please enter the exponent for term " + counter.toString + ": ")
+            var exponent = readLine.toFloat
+
+            //Create an instance of CoAndEx and add it to our ArrayBuffer
+            var temp = new CoAndEx(coefficient, exponent)
+            terms.append(temp)
+
+            counter = counter + 1
+            
+            print("Do you have more terms to enter (y or n): ")
+            answer = readLine
         }
 
+        println(" ")
+
+        print("Please enter the lower bound: ")
+        var lowerbound = readLine.toFloat
+
+        println("Please enter the upper bound: ")
+        var upperbound = readLine.toFloat
         
     } //End of Main
     //Creates a new Class called CoAndEx which takes two float parameters
@@ -36,5 +57,13 @@ object Source {
         }
     }
 
+    def functionCreator(terms: ArrayBuffer[CoAndEx]) : String = {
+        var retval = "f(x) = "
+        for( i <- 0 until (terms.size()-1)){
+            if(terms(i).coefficient == 0){
+                retval = retval + "0 + "
+            }
+        }
+    }
 
 }//End of Source
